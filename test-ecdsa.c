875417948827208dbffc2c09737ad9dfd40f386b
Bart Coppens <kde@bartcoppens.be>
2011-11-22 23:05:42 +0100

    Set up a fake random number generator

--- test-ecdsa.c
+++ test-ecdsa.c
@@ -1,13 +1,13 @@
-#include <openssl/ecdsa.h>
-#include <openssl/objects.h>
-#include <openssl/crypto.h>
-#include <openssl/ec.h>
-#include <openssl/ecdh.h>
 #include <openssl/bn.h>
-#include <openssl/evp.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/err.h>
+#include <openssl/objects.h>
+#include <openssl/rand.h>
 #include <openssl/sha.h>
-#include <string.h>
+#include <execinfo.h>
 #include <stdio.h>
+#include <string.h>
 
 static __inline__ unsigned long long rdtsc(void)
 {
@@ -17,6 +17,47 @@ static __inline__ unsigned long long rdtsc(void)
 }
 
 static const char* test_string = "This is a message to be signed";
+
+#define CALIBRATION
+
+#ifdef CALIBRATION
+
+RAND_METHOD fake_random;
+const RAND_METHOD* old_random;
+
+// We have a fake random generator to ensure that we can force the EC-DSA code to get randomness of a certain form to calibrate our timings
+
+// Implement a fake random number generator, generating specified randomness for the nonces for ECDSA
+int next_fake_random(unsigned char *buf, int bytes) {
+    // Just some debugging for now
+    printf("Generating randomness for %i bytes!\n", bytes);
+
+    // Currently just use the old one for everything
+    return old_random->bytes(buf, bytes);
+}
+
+// Set our fake random generator
+void set_fake_random() {
+    /* the old rand method */
+    old_random = RAND_get_rand_method();
+
+    if (!old_random)
+        return;
+
+    fake_random = *old_random;
+    /* Set a custom random generation function */
+    fake_random.bytes = next_fake_random;
+
+    RAND_set_rand_method(&fake_random);
+}
+
+#else /* CALIBRATION */
+
+// In the real executions, we of course don't fake randomness!
+void set_fake_random() {}
+
+#endif
+
 
 /*
     NID_sect163k1 is the NIST Binary-Curve K-163
@@ -33,6 +74,9 @@ int main(int argc, char** argv) {
     int i;
 
     FILE* theoutputfile = fdopen(1, "w");
+
+    // Potentially set up a fake random generator
+    set_fake_random();
 
     // Digest the string to sign with SHA-1
     SHA1(test_string, strlen(test_string), digest);
